580685b7db68cd8d41c52d2c2129f2f94bd5c2d1
fairtrade051-dotcom <fairtrade051@gmail.com>
2026-03-02 23:40:22 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,11 +11,11 @@ cp /workspace/my_template_repo/app.py /workspace/ComfyUI-SeedVR2_VideoUpscaler/
 
 cd /workspace/ComfyUI-SeedVR2_VideoUpscaler
 
-# 3. อัปเกรด PyTorch ให้เป็นเวอร์ชัน 2.4+ (ตามที่คุณบอกเป๊ะๆ)
+# 3. อัปเกรด PyTorch ให้เป็นเวอร์ชัน 2.4+
 pip install torch torchvision torchaudio --upgrade --index-url https://download.pytorch.org/whl/cu121
 
-# 4. ติดตั้ง Dependencies (เพิ่ม omegaconf เข้าไปแล้ว)
-pip install einops safetensors "diffusers==0.29.2" transformers accelerate pillow gradio opencv-python-headless rotary-embedding-torch omegaconf
+# 4. ติดตั้ง Dependencies (เพิ่ม gguf เข้าไปท้ายสุดแล้ว)
+pip install einops safetensors "diffusers==0.29.2" transformers accelerate pillow gradio opencv-python-headless rotary-embedding-torch omegaconf gguf
 
 # 5. รันหน้าเว็บ
 python app.py
